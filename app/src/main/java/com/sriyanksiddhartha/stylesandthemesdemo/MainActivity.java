34e56998814d85d68d3bdc2e4b41602ba10e2695
SinaSystem <sinasystem47@gmail.com>
2020-06-05 12:41:22 +0430

    Initial commit

---
+++ app/src/main/java/com/sriyanksiddhartha/stylesandthemesdemo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.sriyanksiddhartha.stylesandthemesdemo;
+
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Author: Sriyank Siddhartha
+ *
+ * Module 4: Applying Themes
+ *
+ * 		"AFTER" Demo Project
+ * */
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+
+	}
+}
